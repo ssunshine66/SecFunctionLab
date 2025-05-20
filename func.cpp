1be59cb9185897147b8b3366090050976a7d9e5c
ssunshine66 <vkornienko63@gmail.com>
2025-05-20 18:51:00 +0300

    Implement full FuncA with comments

--- func.cpp
+++ func.cpp
@@ -1,5 +1,13 @@
 #include "func.h"
-
-double TrigFunc::FuncA(double x, int n){
-    return 0; // Поки що нічого не обчислюємо
+#include <cmath>
+#include <vector>
+// Перші числа Ейлера (E_{2n})
+static const std::vector<int> eulerNumbers = {1, -1, 5, -61, 1385, -50521};
+double TrigFunc::FuncA(double x, int n) {
+    double result = 0.0;
+    for (int i=0; i < n && i < eulerNumbers.size(); ++i){
+        double term = (pow (-1, i) * eulerNumbers[i] * pow(x, 2 * i)) / tgamma(2 * i+1);
+        result += term;
+    }
+    return result;
 }
