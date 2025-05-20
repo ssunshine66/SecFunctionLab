389d59d532d46a52f8c501b6455dd31c9a3c43fa
ssunshine66 <vkornienko63@gmail.com>
2025-05-20 18:47:45 +0300

    Hotfix: FuncA with 3 terms only

--- func.cpp
+++ func.cpp
@@ -1,5 +1,13 @@
 #include "func.h"
+#include <cmath>
+#include <vector>
 
-double TrigFunc::FuncA(double x, int n){
-    return 0; // Поки що нічого не обчислюємо
+double TrigFunc::FuncA(double x, int n) {
+    static const std::vector<int> eulerNumbers = {1, -1, 5};
+    double result = 0.0;
+    for (int i = 0; i < 3; ++i) {
+        double term = (pow(-1, i) * eulerNumbers[i] * pow(x, 2 * i)) / tgamma(2 * i + 1);
+        result += term;
+    }
+    return result;
 }
